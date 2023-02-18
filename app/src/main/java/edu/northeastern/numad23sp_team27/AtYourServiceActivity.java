cce62f051f357434c21e42ba7ff550ceb99dbfd3
jerry-asala <asala.j@northeastern.edu>
2023-02-17 16:29:16 -0800

    create new 'AtYourService' activity

---
+++ app/src/main/java/edu/northeastern/numad23sp_team27/AtYourServiceActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
