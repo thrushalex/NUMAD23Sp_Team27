283fca21575184d00857a3df220c47381452a414
butterman0613 <chen.zheng-@northeastern.edu>
2023-03-01 23:27:09 -0800

    add firebase reference
    add firebase reference

--- app/src/main/java/edu/northeastern/numad23sp_team27/StickerActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/StickerActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StickerActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class StickerActivity extends AppCompatActivity {
+    private static final String DB_ADDRESS = "https://at-your-service-4ab17-default-rtdb.firebaseio.com";
+    private DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker);
 
-
+        db = FirebaseDatabase.getInstance(DB_ADDRESS).getReference();
+        db.setValue("firebase demo");
     }
 }
