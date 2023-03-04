25668d0fd5342afd55fb0f4d2483318eb1fe0095
thrushalex <thrushalex@gmail.com>
2023-03-04 18:25:28 -0500

    Refactor createUser and loginUser methods in StickerActivity.java

--- app/src/main/java/edu/northeastern/numad23sp_team27/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/SendStickerActivity.java
@@ -3,12 +3,21 @@ package edu.northeastern.numad23sp_team27;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SendStickerActivity extends AppCompatActivity {
+
+    String logged_in_username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_sticker);
+        logged_in_username = getIntent().getStringExtra("logged_in_username");
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        usernameTextView.setText(logged_in_username);
     }
 }
