3dffe433a33ba0116e49c805bb81e20a24fe7d5e
jerry-asala <asala.j@northeastern.edu>
2023-03-04 11:45:46 -0800

    add User class

---
+++ app/src/main/java/edu/northeastern/numad23sp_team27/User.java
@@ -0,0 +1,21 @@
+package edu.northeastern.numad23sp_team27;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    private String username;
+    private Map<String, Integer> countOfStickersSent;
+    private ArrayList<Integer> historyOfStickersReceived;
+
+    User(String username) {
+        this.username = username;
+
+        // count of stickers sent
+        this.countOfStickersSent = new HashMap<>();
+
+        // history of stickers received
+        this.historyOfStickersReceived = new ArrayList<>();
+    }
+}
