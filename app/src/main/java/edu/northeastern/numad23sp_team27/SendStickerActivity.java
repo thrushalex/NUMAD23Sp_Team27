1193c9d8fefbecacf8bf56b38f0f30eeeb76020f
jerry-asala <asala.j@northeastern.edu>
2023-03-04 14:45:47 -0800

    add username to SharedPreferences on login and retrieve username on user's page

--- app/src/main/java/edu/northeastern/numad23sp_team27/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/SendStickerActivity.java
@@ -2,13 +2,19 @@ package edu.northeastern.numad23sp_team27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SendStickerActivity extends AppCompatActivity {
+    TextView userPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_sticker);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("application", MODE_PRIVATE);
+        String usernm = sharedPreferences.getString("name", "");
     }
 }
