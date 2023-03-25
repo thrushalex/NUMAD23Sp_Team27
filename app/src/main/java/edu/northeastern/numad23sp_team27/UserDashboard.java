11aa627bd0505468074fca0e4919e2ae6099e3d1
jerry-asala <asala.j@northeastern.edu>
2023-03-25 01:47:34 -0700

    get DisplayName of current user and set to welcomeTV

--- app/src/main/java/edu/northeastern/numad23sp_team27/UserDashboard.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/UserDashboard.java
@@ -3,12 +3,26 @@ package edu.northeastern.numad23sp_team27;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserDashboard extends AppCompatActivity {
+
+    FirebaseUser user;
+    TextView welcomeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        welcomeTV = findViewById(R.id.welcomeTV);
+
+        String greeting = "Welcome, " + user.getDisplayName();
+
+        welcomeTV.setText(greeting);
     }
 }
