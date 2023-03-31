c0ff29b388eba3e3dde8c2bf8b49c1fd7d883643
jerry-asala <asala.j@northeastern.edu>
2023-03-31 00:25:45 -0700

    add onClick listener to profile settings button to start new activity

--- app/src/main/java/edu/northeastern/numad23sp_team27/UserDashboard.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/UserDashboard.java
@@ -2,8 +2,11 @@ package edu.northeastern.numad23sp_team27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +22,15 @@ public class UserDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
 
+        Button profileSetBtn = findViewById(R.id.btnProfileSettings);
+
+        profileSetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUserProfileSettings();
+            }
+        });
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         welcomeTV = findViewById(R.id.welcomeTV);
 
@@ -29,5 +41,9 @@ public class UserDashboard extends AppCompatActivity {
 
             welcomeTV.setText(greeting);
         }
+    }
+
+    private void goToUserProfileSettings() {
+        startActivity(new Intent(this, UserProfileSettings.class));
     }
 }
