6f44af2410c3b3641d1472a2740889f546d139d0
jerry-asala <asala.j@northeastern.edu>
2023-03-31 01:53:15 -0700

    implement method to update user display name and email

--- app/src/main/java/edu/northeastern/numad23sp_team27/UserProfileSettings.java
+++ app/src/main/java/edu/northeastern/numad23sp_team27/UserProfileSettings.java
@@ -1,5 +1,6 @@
 package edu.northeastern.numad23sp_team27;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
@@ -7,11 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
-public class UserProfileSettings extends AppCompatActivity {
+public class UserProfileSettings extends AppCompatActivity implements UpdateNameEmailDialog.NameEmailDialogListener {
 
     TextView dispNameTV;
     TextView emailTV;
@@ -49,5 +54,31 @@ public class UserProfileSettings extends AppCompatActivity {
     private void showUpdateDialog() {
         DialogFragment newFrag = new UpdateNameEmailDialog();
         newFrag.show(getSupportFragmentManager(), "updateNameEmail");
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog, String displayName, String email) {
+        updateNameEmail(displayName, email);
+    }
+
+    private void updateNameEmail(String displayName, String email) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            user.updateEmail(email);
+            user.updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(displayName).build()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(UserProfileSettings.this, "User Profile updated successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        dialog.dismiss();
     }
 }
