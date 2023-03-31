a8aec818211891406d98f7d3f43973c952126a8c
jerry-asala <asala.j@northeastern.edu>
2023-03-31 00:12:52 -0700

    add contentDescription to ImageView and create UserProfileSettings activity

---
+++ app/src/main/java/edu/northeastern/numad23sp_team27/UserProfileSettings.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_settings);
+    }
+}
